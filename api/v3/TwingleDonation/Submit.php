e80e548e79eb99fe95a901f24b6c1150b549da5e
Marc Michalsky <michalsky@forumzfd.de>
2024-06-06 15:08:44 +0200

    improve newsletter subcription result values

--- api/v3/TwingleDonation/Submit.php
+++ api/v3/TwingleDonation/Submit.php
@@ -510,7 +510,7 @@ function civicrm_api3_twingle_donation_Submit($params) {
 
     // If usage of double opt-in is selected, use MailingEventSubscribe.create
     // to add contact to newsletter groups defined in the profile
-    $result_values['newsletter']['newsletter_double_opt_in']
+    $result_values['newsletter_double_opt_in']
       = (bool) $profile->getAttribute('newsletter_double_opt_in')
       ? 'true'
       : 'false';
@@ -539,7 +539,7 @@ function civicrm_api3_twingle_donation_Submit($params) {
             ]
           )['visibility'] == 'Public Pages';
         if (!in_array($group_id, $group_memberships, FALSE) && $is_public_group) {
-          $result_values['newsletter'][][$group_id] = civicrm_api3(
+          $result = civicrm_api3(
             'MailingEventSubscribe',
             'create',
             [
@@ -548,9 +548,12 @@ function civicrm_api3_twingle_donation_Submit($params) {
               'contact_id' => $contact_id,
             ]
           );
+          $subscription = CRM_Utils_Array::first($result['values']);
+          $subscription['group_id'] = $group_id;
+          $result_values['newsletter_subscriptions'][] = $subscription;
         }
         elseif ($is_public_group) {
-          $result_values['newsletter'][] = $group_id;
+          $result_values['newsletter_group_ids'][] = $group_id;
         }
       }
       // If requested, add contact to newsletter groups defined in the profile.
@@ -568,8 +571,7 @@ function civicrm_api3_twingle_donation_Submit($params) {
             'contact_id' => $contact_id,
           ]
         );
-
-        $result_values['newsletter'][] = $group_id;
+        $result_values['newsletter_group_ids'][] = $group_id;
       }
     }
 
